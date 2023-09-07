4fec072594f4a9bb3ef1aece8a1c3d4d089ba705
TimoHerrera <therrera@udesa.edu.ar>
2023-09-06 21:59:13 -0300

    error 404

--- src/App.js
+++ src/App.js
@@ -2,16 +2,21 @@ import React from "react";
 import { BrowserRouter,Switch, Route, Link} from "react-router-dom";
 import Home from "./components/Home/Home";
 import MoviesPopularDetail from './components/moviesPopularDetail/MoviesPopularDetail';
-import MoviesRatedDetail from './components/moviesRatedDetail/MoviesRatedDetail'
+import MoviesRatedDetail from './components/moviesRatedDetail/MoviesRatedDetail';
+import Error404 from './components/Error404/Error404';
 
 function App() {
   return (
     <div className="App">  
+
+        
         <Switch>
           <Route path='/' exact component={Home}></Route>
           
           <Route path='/moviesPopular/id/:id' component={MoviesPopularDetail}></Route>
           <Route path='/moviesRated/id/:id' component={MoviesRatedDetail}></Route>
+          <Route component={Error404}/>
+          
           
         </Switch>
 
