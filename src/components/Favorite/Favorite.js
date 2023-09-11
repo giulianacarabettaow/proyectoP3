909ece067c88221b89a23c337e26601a2dde66bf
IgnacioTettamanti <itettamanti@udesa.edu.ar>
2023-09-11 11:18:50 -0300

    .

--- src/components/Favorite/Favorite.js
+++ src/components/Favorite/Favorite.js
@@ -13,17 +13,11 @@ class Favorite extends Component{
         let idFavoritos=[];
         let resultadoPorId =[];
         let recuperoStorage= localStorage.getItem("favoritos")
-        // console.log(recuperoStorage)
 
         
         if (recuperoStorage !==null){
             idFavoritos=JSON.parse(recuperoStorage)
-         
-            // this.setState({
-            //     idfavoritos:JSON.parse(recuperoStorage),
-            // }, console.log(this.state.idfavoritos) )
         
-
             idFavoritos.forEach(unFav => {
                 fetch(`https://api.themoviedb.org/3/movie/${unFav}?api_key=ba0b591fbb4dcbf21e7a279fceca5d5e`)
 
@@ -39,34 +33,33 @@ class Favorite extends Component{
             })
         }
     }
+
+    componentDidUpdate(){
+        // aca hay que ir recargando
+        
+
+
+    }
+
     
 
     render(){
-        //console.log(this.state.idfavoritos)
         console.log(this.state.objFav)
         return(
-            <h2>hiav</h2>
-            // <React.Fragment>
-            // <section className="section1">
-            // <article  className="articlePeliculas">
-            // <h2 className= "tituloPrincipalHome">Lo más visto en películas</h2>
-
-            //     <ul className="categoriesMasVistas">
-            //      {this.state.favoritos.map ((popular,idx)=> <MoviesPopular key={popular.id} name={popular.title} descripcion={popular.overview} id={popular.id}  />  )}                
-            //     </ul>                    
-
-            //  </article>
-            /* <article  className="articlePeliculas">
-            <h2 className= "tituloPrincipalHome">Lo más valorado películas</h2>
+            <React.Fragment>
+            <section className="section1">
+            <article  className="articlePeliculas">
+            <h2 className= "tituloPrincipalHome">Lo más visto en películas</h2>
 
                 <ul className="categoriesMasVistas">
-                 {this.state.moviesTopRated.slice(0,5).map ((rated,idx)=> <MoviesRated key={rated.id} name={rated.title} descripcion={rated.overview} id={rated.id}  />  )}                
+                 {this.state.objFav.map ((popular,idx)=> <MoviesPopular key={popular.id} name={popular.title} descripcion={popular.overview} id={popular.id}  />  )}                
                 </ul>                    
 
-            </article> */
+             </article>
 
-            // </section>
-            // </React.Fragment>
+            </section>
+            </React.Fragment>
+
         )
     }
       
