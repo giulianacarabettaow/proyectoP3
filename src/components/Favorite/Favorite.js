ca39e79b2ac5daeb3dc1483dc4e1459d4c08ea1b
IgnacioTettamanti <itettamanti@udesa.edu.ar>
2023-09-11 11:41:58 -0300

    :

--- src/components/Favorite/Favorite.js
+++ src/components/Favorite/Favorite.js
@@ -37,11 +37,12 @@ class Favorite extends Component{
     componentDidUpdate(){
         // aca hay que ir recargando
         
+        
+    }   
 
-
+    eliminarFav(id){
+        this.state.objFav.filter( unFav => unFav.id !== id)
     }
-
-    
 
     render(){
         console.log(this.state.objFav)
