b648233302ee58d0ea3b10ee7fc2ce1daf4d596d
giulianacarabettaow <gcarabettaowens@udesa.edu.ar>
2023-09-11 15:42:24 -0300

    movies rated y css algo

--- src/components/moviesRated/MoviesRated.js
+++ src/components/moviesRated/MoviesRated.js
@@ -1,7 +1,59 @@
-import React, {Component} from "react";
-import {Link} from "react-router-dom";
-import '../../styles.css';
+// import React, {Component} from "react";
+// import {Link} from "react-router-dom";
+// import '../../styles.css';
 
+
+// class MoviesRated extends Component{
+//     constructor(props){
+//         super(props)
+//         this.state={
+//             extra: false,
+//             ButtonFavs:"Agregar a favoritos",
+//             favoritos:[],
+//         }
+        
+//     }
+
+    
+//     verDescricion(){
+//         this.setState({
+//             extra: true
+//     })}
+
+//     noVerDescricion(){
+//         this.setState({
+//             extra: false
+//     })}
+
+//     render(){
+//         return(
+//         <React.Fragment>
+//             <img/>
+//             <li>Name:{this.props.name} </li>
+           
+//              <li>{ this.state.extra ? this.props.descripcion : ''} </li>
+//              {
+//                 this.state.extra ? 
+
+//                     <button type="button" onClick={()=>this.noVerDescricion()} >Ocultar</button>
+//                 :
+//                     <button type="button" onClick={()=>this.verDescricion()} >Ver descipcion</button>
+
+//              }
+//             <li><Link to={`/moviesRated/id/${this.props.id}`}><button type='button'>Ir al detalle</button></Link></li>
+             
+//              {/* <li><button onClick={this.agregarFavs(this.props.id)}type="button">{this.state.ButtonFavs}</button></li> */}
+
+//         </React.Fragment>
+//         )
+//     }
+
+// }
+
+// export default MoviesRated
+import React, {Component} from "react";
+import {Link} from 'react-router-dom';
+import '../../styles.css';
 
 class MoviesRated extends Component{
     constructor(props){
@@ -10,11 +62,34 @@ class MoviesRated extends Component{
             extra: false,
             ButtonFavs:"Agregar a favoritos",
             favoritos:[],
+            id:props.id 
         }
-        
+         console.log(this.state)
     }
 
+     componentDidMount(){
+          
+         let arrayFavoritos=[];
+         
+         let recuperoStorage= localStorage.getItem("favoritos")
+
+          if (recuperoStorage !==null){
     
+              arrayFavoritos=JSON.parse(recuperoStorage);
+              this.setState({
+                  favoritos: arrayFavoritos
+              })
+
+              if (arrayFavoritos.includes(this.state.id)){
+                  this.setState({
+                      ButtonFavs:"Quitar de favoritos"
+                  })
+              }
+          } 
+     }
+
+
+
     verDescricion(){
         this.setState({
             extra: true
@@ -24,30 +99,66 @@ class MoviesRated extends Component{
         this.setState({
             extra: false
     })}
+    
+     agregarFavs(id){
+         console.log(id)
+    
+         let arrayFavoritos=[];
+         let recuperoStorage= localStorage.getItem("favoritos");
+
+         if (recuperoStorage !==null){
+             arrayFavoritos=JSON.parse(recuperoStorage);
+         }
+        
+         if (arrayFavoritos.includes(id)){
+             arrayFavoritos=arrayFavoritos.filter(unId => unId !==id) 
+
+             this.setState({
+            favoritos:arrayFavoritos,
+             ButtonFavs: "Agregar a favoritos"
+         })
+    
+          }else{
+
+             arrayFavoritos.push(id);
+             this.setState({
+             favoritos:arrayFavoritos,
+             ButtonFavs:"Quitar de favoritos"
+         })
+         }
+            let arrayFavoritosAString = JSON.stringify(arrayFavoritos)
+            localStorage.setItem('favoritos', arrayFavoritosAString)
+            console.log(arrayFavoritos);
+
+            if(this.props.handle){
+                this.props.handle()
+            }
+        }
 
     render(){
         return(
         <React.Fragment>
             <img/>
-            <li>Name:{this.props.name} </li>
-           
-             <li>{ this.state.extra ? this.props.descripcion : ''} </li>
-             {
-                this.state.extra ? 
+            <ul className="listaPelis">
+                <li>Name:{this.props.name} </li>
+            
+                <li>{ this.state.extra ? this.props.descripcion : ''} </li>
+                {
+                    this.state.extra ? 
 
-                    <button type="button" onClick={()=>this.noVerDescricion()} >Ocultar</button>
-                :
-                    <button type="button" onClick={()=>this.verDescricion()} >Ver descipcion</button>
+                        <button className="botonesHome" type="button" onClick={()=>this.noVerDescricion()} >Ocultar</button>
+                    :
+                        <button  className="botonesHome" type="button" onClick={()=>this.verDescricion()} >Ver descipcion</button>
 
-             }
-            <li><Link to={`/moviesRated/id/${this.props.id}`}><button type='button'>Ir al detalle</button></Link></li>
-             
-             {/* <li><button onClick={this.agregarFavs(this.props.id)}type="button">{this.state.ButtonFavs}</button></li> */}
+                }
+                <li><Link to={`/moviesPopular/id/${this.props.id}`}><button className="botonesHome" type='button'>Ir al detalle</button></Link></li>
+                
+                <li><button className="botonesHome" onClick={()=>this.agregarFavs(this.props.id)}type="button">{this.state.ButtonFavs}</button></li>
+            </ul>
 
         </React.Fragment>
         )
     }
-
 }
 
 export default MoviesRated
