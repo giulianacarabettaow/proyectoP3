022ad334eca5e333b09ab0a4b414e83e1f8b3f87
giulianacarabettaow <gcarabettaowens@udesa.edu.ar>
2023-09-15 22:09:58 -0300

    .....

--- src/components/Error404/Error404.js
+++ src/components/Error404/Error404.js
@@ -2,9 +2,9 @@ import '../../styles.css';
 
 function Error404() {
     return(
-        <div>
+        <div className='error'>
             <h2>404 NOT FOUND GIL</h2>
-            <img src="./img/walter.gif"></img>
+            <img src="./img/walter.gif" className='gifError'></img>
         </div>
     )
     
